5728a06ed456a7b18fbf90e85eeb1eccf49c4c26
최중재 / Jungjae Choi <joong8812@gmail.com>
2022-04-20 17:38:28 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,25 @@
+def count_word_in_the_string(word):
+    croatia_words = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+    count = 0
+
+    if not word.strip():
+        return count
+
+    for char in croatia_words:
+        pos = word.find(char)
+        if pos > -1:
+            word = word.replace(char, " ", 1)
+            count += 1
+            break
+
+    left_characters = word.strip()
+    if count == 0 and len(left_characters):
+        left_characters = left_characters.replace(" ", "")
+        count += len(left_characters)
+        word = ""
+
+    return count_word_in_the_string(word) + count
+
+
+input_word = input()
+print(count_word_in_the_string(input_word))
