e33a9ed6bebc39656c553ddab17c4bea32ec431a
최중재 / Jungjae Choi <joong8812@gmail.com>
2022-05-14 23:56:02 +0900

    [Bronze II] Title: 블랙잭, Time: 180 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const NM = input
+  .shift()
+  .split(" ")
+  .map((num) => parseInt(num));
+const N = NM.shift();
+const M = NM.shift();
+const cardArr = input
+  .shift()
+  .split(" ")
+  .map((num) => parseInt(num));
+let max = 0;
+
+for (let i = 0; i < N - 2; i++) {
+  for (let j = i + 1; j < N - 1; j++) {
+    for (let k = j + 1; k < N; k++) {
+      let sum = cardArr[i] + cardArr[j] + cardArr[k];
+      if (sum > max && sum <= M) {
+        max = sum;
+      }
+    }
+  }
+}
+
+console.log(max);
